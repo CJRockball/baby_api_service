d2f11424e8ce89f519a2d67d83040974f5feb2a4
Patrick Carlberg <11623836+CJRockball@users.noreply.github.com>
2022-01-11 13:31:17 +0800

    added logs

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
+import logging
+import pathlib
 from typing import Optional
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
+PROJECT_PATH = pathlib.Path(__file__).resolve().parent.parent
+LOG_FILE = PROJECT_PATH / "logs/info.log"
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+)
+
 
 @app.get("/")
-def get_home(q: Optional[str] = None):
-    return {"input": q}
+def index_get():
+    logging.info("Opened landing page")
+    return "Landing Page"
 
